9171abc2136aae0d1aad0d98ea0c85ed074afb5b
alicia <aliciafdezrov@gmail.com>
2025-05-25 13:03:15 +0200

    Add new fields to meals content

--- app/(tabs)/(meals)/components/MealsContent.tsx
+++ app/(tabs)/(meals)/components/MealsContent.tsx
@@ -1,15 +1,19 @@
 import { getFoods } from '@/features/food/services/Database';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useSQLiteContext } from 'expo-sqlite';
 import AddFoodForm from '@/pods/meals/addFoodForm/AddFoodForm.component';
 import { Food } from '@/features/food/models/Food.model';
 import { MealFood } from '@/src/pods/meals/MealFood.vm';
 import FoodTable from '@/src/pods/meals/foodTable/FoodTable.component';
+import { TextInput } from '@/src/components/ui/TextInput';
+import { Button } from '@/src/components/ui/Button';
+import { StyleSheet } from 'react-native';
 
 export function MealsContent() {
   const [foods, setFoods] = useState<Food[]>([]);
   const db = useSQLiteContext();
   const [selectedFoods, setSelectedFoods] = useState<MealFood[]>([]);
+  const [mealName, setMealName] = useState<string>('');
 
   useEffect(() => {
     const loadFoods = async () => {
@@ -24,10 +28,41 @@ export function MealsContent() {
     setSelectedFoods((prevFoods) => [...prevFoods, mealFood]);
   };
 
+  const handleChangeName = (name: string) => {
+    setMealName(name);
+  };
+
+  const disableSaveButton = useMemo(() => {
+    return mealName.length === 0 || selectedFoods.length === 0;
+  }, [mealName, selectedFoods]);
+
+  const onSave = () => {
+    console.log('save');
+  };
+
   return (
     <>
+      <TextInput
+        label="Nombre comida"
+        value={mealName}
+        onChangeText={handleChangeName}
+      />
       <AddFoodForm foods={foods} addSelectedFood={handleAddMealFood} />
       <FoodTable mealFoodList={selectedFoods} />
+      <Button
+        variant="primary"
+        size="large"
+        title="Guardar"
+        onPress={onSave}
+        style={styles.button}
+        disabled={disableSaveButton}
+      />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'flex-end',
+  },
+});
