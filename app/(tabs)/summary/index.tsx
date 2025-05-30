671f76770d9622f717aa8f0caae2c916223f7eff
alicia <aliciafdezrov@gmail.com>
2025-05-30 13:36:30 +0200

    Added custom image for summary header

--- app/(tabs)/summary/index.tsx
+++ app/(tabs)/summary/index.tsx
@@ -10,7 +10,7 @@ export default function SummaryScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/analytics.jpg')}
           style={styles.reactLogo}
         />
       }
@@ -23,10 +23,7 @@ export default function SummaryScreen() {
 
 const styles = StyleSheet.create({
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    height: '100%',
+    width: '100%',
   },
 });
