231037cc78e445556b2218b5824784b35847eb51
alicia <aliciafdezrov@gmail.com>
2025-05-30 20:12:04 +0200

    Added label variant to ThemedText

--- src/components/ThemedText.tsx
+++ src/components/ThemedText.tsx
@@ -5,7 +5,13 @@ import { useThemeColor } from '../hooks/useThemeColor';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?:
+    | 'default'
+    | 'title'
+    | 'defaultSemiBold'
+    | 'subtitle'
+    | 'link'
+    | 'label';
 };
 
 export function ThemedText({
@@ -26,6 +32,7 @@ export function ThemedText({
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
         type === 'subtitle' ? styles.subtitle : undefined,
         type === 'link' ? styles.link : undefined,
+        type === 'label' ? styles.label : undefined,
         style,
       ]}
       {...rest}
@@ -56,5 +63,10 @@ const styles = StyleSheet.create({
     lineHeight: 30,
     fontSize: 16,
     color: '#0a7ea4',
+  },
+  label: {
+    fontSize: 12,
+    opacity: 0.7,
+    lineHeight: 24,
   },
 });
